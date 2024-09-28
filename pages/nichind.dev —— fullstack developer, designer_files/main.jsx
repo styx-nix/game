1466adae5893b8f19585170618653134ae511a21
beer <abeerarora0@gmail.com>
2024-09-28 13:10:24 -0400

    Add files via upload

---
+++ pages/nichind.dev —— fullstack developer, designer_files/main.jsx
@@ -0,0 +1,13 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=055e9053"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=122de945"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=2cd57713"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/src/App.jsx?t=1721135339455";
+import "/src/styles/index.css";
+ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+    fileName: "/home/web/NichWeb/NichindWeb/src/main.jsx",
+    lineNumber: 8,
+    columnNumber: 3
+}, this)) // NO STRICT
+;
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC5qc3gnXG5pbXBvcnQgJy4vc3R5bGVzL2luZGV4LmNzcydcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG5cbiAgPEFwcCAvPlxuXG4pXG4vLyBOTyBTVFJJQ1QiXSwibmFtZXMiOlsiUmVhY3QiLCJSZWFjdERPTSIsIkFwcCIsImNyZWF0ZVJvb3QiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sIm1hcHBpbmdzIjoiO0FBQUEsT0FBT0EsV0FBVyxRQUFPO0FBQ3pCLE9BQU9DLGNBQWMsbUJBQWtCO0FBQ3ZDLE9BQU9DLFNBQVMsWUFBVztBQUMzQixPQUFPLHFCQUFvQjtBQUUzQkQsU0FBU0UsVUFBVSxDQUFDQyxTQUFTQyxjQUFjLENBQUMsU0FBU0MsTUFBTSxlQUV6RCxRQUFDSjs7OztVQUdILFlBQVkifQ==
